41ce958899a77850fae555cfc0ff82f733cf61d4
Justin Chen <yokogaisha@gmail.com>
2025-11-10 09:11:05 +0900

    chore: add Capacitor dependencies for Android and iOS support

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.justincourse.nuxtbase',
+  appName: 'nuxt-base-app',
+  webDir: 'dist'
+};
+
+export default config;
