a2fb99c71fec97d2cf7e767c3fc21b3cc0a4118c
Justin Chen <yokogaisha@gmail.com>
2025-11-10 09:39:28 +0900

    fix: remove iOS open command from mobile script

--- mobile.sh
+++ mobile.sh
@@ -4,4 +4,3 @@ npm install
 npm run generate
 
 npx cap sync
-npx cap open ios
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false, // 关键
+  nitro: {
+    preset: 'static'
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@nuxt/content']
